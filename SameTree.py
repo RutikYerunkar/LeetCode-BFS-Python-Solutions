7b69bb34470f9ad67a8c1ca0cacb7680b98763e2
RutikYerunkar <73027226+RutikYerunkar@users.noreply.github.com>
2024-01-21 08:25:35 -0800

    Same Tree Solution

---
+++ SameTree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from queue import SimpleQueue
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        queue = SimpleQueue()
+        queue.put(p)
+        queue.put(q)
+
+        while not queue.empty():
+            first = queue.get()
+            second = queue.get()
+
+            if not first and not second:
+                continue
+            elif not first or not second or first.val != second.val:
+                return False
+            
+            queue.put(first.left)
+            queue.put(second.left)
+            queue.put(first.right)
+            queue.put(second.right)
+
+        return True
+        
